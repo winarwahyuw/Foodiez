14ef3aacec8c256ae7648944d571f44f4fb25c93
winarwahyuw <winarwahyuw@gmail.com>
2023-06-05 11:08:34 +0700

    change style css to scss

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
 import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
+import '../styles/main.scss';
 import './components/cardElement'
 import data from '../DATA.json' ;
 
