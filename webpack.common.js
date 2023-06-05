c43e3c552e09187a165929289ea9b3d71d004e49
winarwahyuw <winarwahyuw@gmail.com>
2023-06-05 11:08:58 +0700

    change style css to scss

--- webpack.common.js
+++ webpack.common.js
@@ -15,14 +15,11 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.scss$/,
         use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-          },
+          'style-loader',
+          'css-loader',
+          'sass-loader',
         ],
       },
     ],
