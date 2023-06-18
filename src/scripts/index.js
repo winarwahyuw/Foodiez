613b001324bb05166ed2b866bd358a47fca486db
winarwahyuw <winarwahyuw@gmail.com>
2023-06-18 11:53:36 +0700

    handle eslint airbnb code styles

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,49 +1,50 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.scss';
-import './components/cardElement'
-import data from '../DATA.json' ;
+import './components/cardElement';
+import data from '../DATA.json';
 
 const { restaurants } = data;
-const PopularSection = document.getElementById("popular");
+const PopularSection = document.getElementById('popular');
+const btnDropdown = document.getElementById('btn-dropdown');
+const menuDropdown = document.getElementById('dropdown-menu');
+const nextBtn = document.getElementById('right-arrow');
+const prevBtn = document.getElementById('left-arrow');
+const slider = document.getElementById('popular');
 
-restaurants.forEach(resto => {
-    let CardElement = document.createElement("card-element");
+restaurants.forEach((resto) => {
+  const CardElement = document.createElement('card-element');
 
-    CardElement.setAttribute("id", resto.id)
-    CardElement.setAttribute("image", resto.pictureId)
-    CardElement.setAttribute("name", resto.name)
-    CardElement.setAttribute("rating", resto.rating)
-    CardElement.setAttribute("city", resto.city)
+  CardElement.setAttribute('card-element', resto.id);
+  CardElement.setAttribute('image', resto.pictureId);
+  CardElement.setAttribute('name', resto.name);
+  CardElement.setAttribute('rating', resto.rating);
+  CardElement.setAttribute('city', resto.city);
 
-    PopularSection.appendChild(CardElement)
+  PopularSection.appendChild(CardElement);
 });
 
-let btnDropdown = document.getElementById("btn-dropdown")
-let menuDropdown = document.getElementById("dropdown-menu")
-btnDropdown.addEventListener("click", function(e){
-    if(menuDropdown.classList.contains("show")){
-        menuDropdown.classList.remove("show")
-        menuDropdown.classList.add("hide")
-    }else{
-        menuDropdown.classList.remove("hide")
-        menuDropdown.classList.add("show")
-    }
-})
+btnDropdown.addEventListener('click', (e) => {
+  e.stopPropagation();
+  if (menuDropdown.classList.contains('show')) {
+    menuDropdown.classList.remove('show');
+    menuDropdown.classList.add('hide');
+  } else {
+    menuDropdown.classList.remove('hide');
+    menuDropdown.classList.add('show');
+  }
+});
 
-const scrollLeft = function () {  
-    setTimeout(function () {
+function scrollLeft() {
+  setTimeout(() => {
     slider.scrollTo(slider.scrollLeft + 600, window.scrollY);
-    }, 10);
-};
+  }, 10);
+}
 
-const scrollRight = function () {  
-    setTimeout(function () {
+function scrollRight() {
+  setTimeout(() => {
     slider.scrollTo(slider.scrollLeft - 600, window.scrollY);
-    }, 10);
-};
+  }, 10);
+}
 
-let nextBtn = document.getElementById("right-arrow")
-let prevBtn = document.getElementById("left-arrow")
-let slider = document.getElementById("popular")
-nextBtn.addEventListener("click", scrollLeft);
-prevBtn.addEventListener("click", scrollRight);
+nextBtn.addEventListener('click', scrollLeft);
+prevBtn.addEventListener('click', scrollRight);
