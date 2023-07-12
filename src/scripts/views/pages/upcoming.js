e22079e85bf1257cb649f110422c96f676c45c6f
winarwahyuw <winarwahyuw@gmail.com>
2023-07-12 15:32:54 +0700

    handle routing dan api

---
+++ src/scripts/views/pages/upcoming.js
@@ -0,0 +1,11 @@
+const Upcoming = {
+  async render () {
+    return `
+        <h2>Upcoming page</h2>
+      `
+  },
+  async afterRender () {
+    // Fungsi ini akan dipanggil setelah render()
+  }
+}
+export default Upcoming
