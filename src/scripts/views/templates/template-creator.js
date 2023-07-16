042b475778ec436201ccf5581e55ef70cd4c8c07
winarwahyuw <winarwahyuw@gmail.com>
2023-07-16 22:15:38 +0700

    fixing min-height 44px

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -8,7 +8,7 @@ const createRestaurantItem = (restaurant) => `
         <p><i class="fa fa-star icon-secondary"></i> ${restaurant.rating}</p>
     </div>
     <div class="card-footer d-flex-row">
-        <a class="btn-sm btn-secondary" href="/#/detail/${restaurant.id}">Check this Out</a>
+        <a class="btn btn-secondary" href="/#/detail/${restaurant.id}">Check this Out</a>
     </div>
   </div>
 `
