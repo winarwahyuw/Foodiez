e7a4b2ff13c037cd4eb7365aa4b8cc0833e61f70
winarwahyuw <winarwahyuw@gmail.com>
2023-07-27 15:45:36 +0700

    search fav restaurants done

--- src/scripts/views/pages/liked-restaurants/favorite-restaurant-search-presenter.js
+++ src/scripts/views/pages/liked-restaurants/favorite-restaurant-search-presenter.js
@@ -11,9 +11,20 @@ class FavoriteRestaurantSearchPresenter {
     })
   }
 
-  _searchRestaurants (latestQuery) {
+  async _searchRestaurants (latestQuery) {
     this._latestQuery = latestQuery
-    this._favoriteRestaurants.searchRestaurants(this._latestQuery)
+
+    const foundRestaurants = await this._favoriteRestaurants.searchRestaurants(this._latestQuery)
+
+    this._showFoundRestaurants(foundRestaurants)
+  }
+
+  _showFoundRestaurants (restaurants) {
+    const html = restaurants?.reduce(
+      (carry, restaurant) => carry.concat(`<li class="restaurant"><span class="restaurant__title">${restaurant.title || '-'}</span></li>`), '')
+    document.querySelector('.restaurants').innerHTML = html
+    document.getElementById('restaurant-search-container')
+      .dispatchEvent(new Event('restaurants:searched:updated'))
   }
 
   get latestQuery () {
