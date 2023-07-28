42eb51798ea1208dd4610508deebffa01b30b4dd
winarwahyuw <winarwahyuw@gmail.com>
2023-07-28 10:43:57 +0700

    pisahkan berkas view

--- specs/favoriteRestaurantSearchSpec.js
+++ specs/favoriteRestaurantSearchSpec.js
@@ -1,40 +1,7 @@
 /* eslint-disable no-undef */
 import FavoriteRestaurantSearchPresenter from '../src/scripts/views/pages/liked-restaurants/favorite-restaurant-search-presenter'
 import FavoriteIdb from '../src/scripts/data/favorite.-idb'
-
-class FavoriteRestaurantSearchView {
-  getTemplate () {
-    return `
-    <div id="restaurant-search-container">
-      <input id="query" type="text">
-      <div class="restaurant-result-container">
-          <ul class="restaurants">
-          </ul>
-      </div>
-    </div>
-    `
-  }
-
-  runWhenUserIsSearching (callback) {
-    document.getElementById('query').addEventListener('change', (event) => {
-      callback(event.target.value)
-    })
-  }
-
-  showRestaurants (restaurants) {
-    let html
-    if (restaurants?.length > 0) {
-      html = restaurants.reduce(
-        (carry, restaurant) => carry.concat(`<li class="restaurant"><span class="restaurant__title">${restaurant.title || '-'}</span></li>`), '')
-    } else {
-      html = '<div class="restaurants__not__found">restaurants tidak ditemukan</div>'
-    }
-
-    document.querySelector('.restaurants').innerHTML = html
-
-    document.getElementById('restaurant-search-container').dispatchEvent(new Event('restaurants:searched:updated'))
-  }
-}
+import FavoriteRestaurantSearchView from '../src/scripts/views/pages/liked-restaurants/favorite-restaurant-search-view'
 
 describe('Searching restaurants', () => {
   let presenter
